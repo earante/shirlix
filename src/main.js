52b0af73e4457136e91e2cb642e5e7b446a5fb9e
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-08-11 20:47:05 +0800

    feat: added login page

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import router from './router'
 
 //Components
 import App from './App.vue'
@@ -20,5 +21,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(vuetify)
+app.use(router)
 
 app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LoginView from '../../views/LoginView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+})
+
+export default router
