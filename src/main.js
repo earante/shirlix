6eacbc6c1aee6ad3779b8f8c62d8690d9516f4ee
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-08-11 18:10:27 +0800

    fixed: removing built-in setup, setup vuetify on main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
 import { createPinia } from 'pinia'
+
+//Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//Components
 import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 
 app.mount('#app')
