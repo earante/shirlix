e39d0d4629fdacf796c5b00b49b96e3f73557860
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-08-13 14:21:02 +0800

    feat: updated AppLayout

--- views/auth/LoginView.vue
+++ views/auth/LoginView.vue
@@ -1,69 +1,67 @@
 <script setup>
 import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+const showPassword = ref(false)
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import { useDisplay } from 'vuetify'
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar color="grey-lighten-1" class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-10">
+          <v-card class="mx-auto" elevation="24" color="orange-darken-2">
+            <v-card-title class="text-center">
+              <v-img
+                class="mx-auto"
+                src="/images/logo-favicon.png"
+                :width="mobile ? '75%' : '25%'"
+              ></v-img>
+              <h3 class="font-weight-black">Shirlix Meatshop</h3>
+              <p>Login Form</p>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-divider class="my-5"></v-divider>
+              <v-form fast-fail @submit.prevent>
+                <v-text-field
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+                  prepend-inner-icon="mdi-email"
+                ></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-10">
-              <v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Login" elevation="24">
-                <template v-slot:title>
-                  <span class="font-weight-black">Shirlix Meatshop</span>
-                </template>
+                <v-text-field
+                  label="Password"
+                  variant="outlined"
+                  prepend-inner-icon="mdi-lock"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                  @click:append-inner="showPassword = !showPassword"
+                ></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field
-                      label="Email"
-                      type="email"
-                      variant="outlined"
-                      prepend-inner-icon="mdi-email"
-                    ></v-text-field>
-
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                      prepend-inner-icon="mdi-lock"
-                    ></v-text-field>
-
-                    <v-btn color="primary" class="mt-2" type="submit" block>Login</v-btn>
-                    <v-divider class="my-5"></v-divider>
-                    <h4 class="text-center">
-                      Don't have account?
-                      <RouterLink class="text-primary" to="/register"
-                        >Click here to Register</RouterLink
-                      >
-                    </h4>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="grey-lighten-1" border app>2025 - Shirlix Meatshop</v-footer>
-    </v-app>
-  </v-responsive>
+                <v-btn
+                  color="orange-darken-2"
+                  class="mt-2"
+                  type="submit"
+                  block
+                  prepend-icon="mdi-login"
+                  >Login</v-btn
+                >
+                <v-divider class="my-5"></v-divider>
+                <h4 class="text-center">
+                  Don't have account?
+                  <RouterLink class="text-primary" to="/register"
+                    >Click here to Register.</RouterLink
+                  >
+                </h4>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
--- views/auth/RegisterView.vue
+++ views/auth/RegisterView.vue
@@ -1,69 +1,69 @@
 <script setup>
 import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+const showPassword = ref(false)
+const showConfirmPassword = ref(false)
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import { useDisplay } from 'vuetify'
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <!-- <v-responsive class="border rounded"> -->
-  <v-app :theme="theme">
-    <v-app-bar color="grey-lighten-1" class="px-3">
-      <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-10">
+          <v-card class="mx-auto" elevation="24" color="orange-darken-2">
+            <v-card-title class="text-center">
+              <v-img
+                class="mx-auto"
+                src="/images/logo-favicon.png"
+                :width="mobile ? '75%' : '25%'"
+              ></v-img>
+              <h3 class="font-weight-black">Shirlix Meatshop</h3>
+              <p>Registration Form</p>
+            </v-card-title>
 
-      <v-btn
-        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-        text="Toggle Theme"
-        slim
-        @click="onClick"
-      ></v-btn>
-    </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-divider class="my-5"></v-divider>
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="First Name" type="text" variant="outlined"></v-text-field>
 
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="6" class="mx-auto pt-10">
-            <v-card
-              class="mx-auto"
-              prepend-icon="mdi-account-plus"
-              subtitle="Register"
-              elevation="24"
-            >
-              <template v-slot:title>
-                <span class="font-weight-black">Shirlix Meatshop</span>
-              </template>
+                <v-text-field label="Last Name" type="text" variant="outlined"></v-text-field>
+                <v-text-field label="Email" type="email" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Password"
+                  variant="outlined"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                  @click:append-inner="showPassword = !showPassword"
+                ></v-text-field>
+                <v-text-field
+                  label="Confirm Password"
+                  variant="outlined"
+                  :type="showConfirmPassword ? 'text' : 'password'"
+                  :append-inner-icon="showConfirmPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                  @click:append-inner="showConfirmPassword = !showConfirmPassword"
+                ></v-text-field>
 
-              <v-card-text class="bg-surface-light pt-4">
-                <v-form fast-fail @submit.prevent>
-                  <v-text-field label="First Name" type="text" variant="outlined"></v-text-field>
-
-                  <v-text-field label="Last Name" type="text" variant="outlined"></v-text-field>
-                  <v-text-field label="Email" type="email" variant="outlined"></v-text-field>
-                  <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
-                  <v-text-field
-                    label="Confirm Password"
-                    type="password"
-                    variant="outlined"
-                  ></v-text-field>
-
-                  <v-btn color="primary" class="mt-2" type="submit" block>Register</v-btn>
-                  <v-divider class="my-5"></v-divider>
-                  <h4 class="text-center">
-                    Already have account?
-                    <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
-                  </h4>
-                </v-form>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-
-    <v-footer color="grey-lighten-1" border app>2025 - Shirlix Meatshop</v-footer>
-  </v-app>
-  <!-- </v-responsive> -->
+                <v-btn
+                  color="orange-darken-2"
+                  class="mt-2"
+                  type="submit"
+                  block
+                  prepend-icon="mdi-account-plus"
+                  >Register</v-btn
+                >
+                <v-divider class="my-5"></v-divider>
+                <h4 class="text-center">
+                  Already have account?
+                  <RouterLink class="text-primary" to="/">Click here to Login.</RouterLink>
+                </h4>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
