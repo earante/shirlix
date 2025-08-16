51a10a89446a311f3c4399b6a66f3ad050bf0581
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-08-17 00:31:39 +0800

    feat: added color of buttons; added localStorage of Theme

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 
-const theme = ref('light')
+const theme = ref(localStorage.getItem('theme') ?? 'light')
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
+  localStorage.setItem('theme', theme.value)
 }
 </script>
 
@@ -21,7 +22,8 @@ function onClick() {
         <v-btn
           :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
           slim
-          variant="elevated "
+          variant="elevated"
+          color="orange-darken-2"
           @click="onClick"
         ></v-btn>
       </v-app-bar>
